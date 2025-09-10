196989cfc4145d8a338b58e2eda30a4c1eb48c37
GreenHouse007 <adventurebyalex@gmail.com>
2025-09-10 13:03:14 -0600

    code cleanup

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-/* require("dotenv").config();
-
-const express = require("express");
-const mongoose = require("mongoose");
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Use your MongoDB URI from .env
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error(err));
-
-app.listen(port, () => {
-  console.log(`🚀 Server running on port ${port}`);
-});
-*/
-
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
